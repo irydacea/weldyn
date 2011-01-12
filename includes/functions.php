c9e9e9a41e839de4022d270ff99f4284717d6f4f
Ignacio R. Morelle <shadowm@wesnoth.org>
2011-01-11 23:33:29 -0300

    Alternative hack to log failed login attempts, thanks to Noxwizard

--- includes/functions.php
+++ includes/functions.php
@@ -3006,6 +3006,7 @@ function login_box($redirect = '', $l_explain = '', $l_success = '', $admin = fa
 				if ($result['error_msg'] == 'LOGIN_ERROR_USERNAME' || $result['error_msg'] == 'LOGIN_ERROR_PASSWORD')
 				{
 					$err = (!$config['board_contact']) ? sprintf($user->lang[$result['error_msg']], '', '') : sprintf($user->lang[$result['error_msg']], '<a href="mailto:' . htmlspecialchars($config['board_contact']) . '">', '</a>');
+					add_log('user', $result['user_row']['user_id'], 'Failed login with incorrect password');
 				}
 
 			break;
